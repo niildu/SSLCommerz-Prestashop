0f0dd366d523d0a6a3c360d60dcdb5a11d568410
Md Noor Mohammad Siddique <niildu@gmail.com>
2022-01-16 13:14:30 +0600

    Update sslcommerz.js

--- SSLCOMMERZ/js/lib/sslcommerz.js
+++ SSLCOMMERZ/js/lib/sslcommerz.js
@@ -1,5 +1,11 @@
 let disabled = true;
-document.getElementById("conditions_to_approve[terms-and-conditions]").onchange = function() {
-	document.getElementById("sslczPayBtn").disabled = !disabled;
-	disabled = !disabled;
+let tac_fld = document.getElementById("conditions_to_approve[terms-and-conditions]");
+if (tac_fld) {
+	tac_fld.onchange = function() {
+		let sslpbtn = document.getElementById("sslczPayBtn")
+		if (sslpbtn) {
+			sslpbtn.disabled = !disabled;
+			disabled = !disabled;
+		}
+	}
 }
